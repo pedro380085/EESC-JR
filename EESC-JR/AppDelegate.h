cf1b1f02b229d2c185b0691793b77c0643acfee7
Pedro Goes <pedro.pecanha.goes@gmail.com>
2012-04-09 20:24:58 -0300

    Initial Commit

---
+++ EESC-JR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EESC-JR
+//
+//  Created by Pedro Góes on 09/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ EESC-JR/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  EESC-JR
+//
+//  Created by Pedro Góes on 09/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
